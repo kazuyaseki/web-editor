63d46534c059ce2263b5fb3a989421248f7187cd
kazuya-seki <track613armory@gmail.com>
2018-05-06 17:15:20 +0900

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import './App.css';
+
+import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
 class App extends React.Component {
+  public state = {
+    link: 'https://paper.dropbox.com/'
+  };
+
+  public changeLink = (link: string) => {
+    this.setState({ link });
+  };
+
   public render() {
     return (
       <div className="App">
-        <Sidebar />
+        <Sidebar changeLink={this.changeLink} />
+        <Main link={this.state.link} />
       </div>
     );
   }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import './Sidebar.css';
 
-class Sidebar extends React.Component {
+export default class Sidebar extends React.Component<
+  { changeLink: (link: string) => void },
+  any
+> {
   public render() {
     return (
-      <div className="sidebar">
-        <webview src="http://qiita.com" autosize={true} />
-      </div>
+      <ul className="sidebar">
+        <li
+          className="active"
+          data-link="https://paper.dropbox.com/"
+          onClick={this.changeLink}
+        >
+          Dropbox
+        </li>
+        <li data-link="https://ja.todoist.com/" onClick={this.changeLink}>
+          Todoist
+        </li>
+      </ul>
     );
   }
-}
 
-export default Sidebar;
+  private changeLink = (e: React.MouseEvent<HTMLLIElement>) => {
+    this.props.changeLink(e.currentTarget.getAttribute('data-link') || '');
+  };
+}
