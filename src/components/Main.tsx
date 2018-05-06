b89f768d95c4c8addb3efe54e3016a1c5405beb1
kazuya-seki <track613armory@gmail.com>
2018-05-06 17:15:06 +0900

    added main

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import WebView from './WebView';
+import './Main.css';
+
+export default class Main extends React.Component<{ link: string }, any> {
+  public render() {
+    return (
+      <div className="main">
+        <div className="webview">
+          <WebView src={this.props.link} allowpopups />
+        </div>
+      </div>
+    );
+  }
+}
